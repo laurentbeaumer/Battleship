6446ae265dbaf1dc374f5b42c6ed1fa35e7e577c
Laurent Beaumer <laurent.beaumer@free.fr>
2019-12-04 10:37:39 +1100

    Battleship.Square: create for later use

---
+++ Battleship/Square.cs
@@ -0,0 +1,9 @@
+﻿namespace Battleship
+{
+    public enum Square
+    {
+        Empty,      // default state
+        Occupied,   // ship has been added
+        Hit         // ship has been added
+    }
+}
--- BattleshipConsole/Program.cs
+++ BattleshipConsole/Program.cs
@@ -9,11 +9,19 @@
             bool quit = false;
             var game = new Game();
 
+            // Loop "Player 1" to add ship
+
+            // Loop "Player 2" to add ship
+
+            // Start the attacks with "Player 1"
             do
             {
 
             }
-            while (!quit && game.NextPlayer());
+            while (!quit && game.NextPlayer()); 
+            // Either players decide to quit, or one of other player has lost during the turn
+
+            // Display result to players
         }
     }
 }
