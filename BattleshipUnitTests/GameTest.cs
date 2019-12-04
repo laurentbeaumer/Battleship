fc8b8ab526e99afb3b85a56efe459c2351e5da5a
Laurent Beaumer <laurent.beaumer@free.fr>
2019-12-04 13:16:05 +1100

    BattleshipUnitTests.GameTest: add more tests

--- BattleshipUnitTests/GameTest.cs
+++ BattleshipUnitTests/GameTest.cs
@@ -1,14 +1,41 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Diagnostics;
 
 namespace BattleshipUnitTests
 {
     using Battleship;
 
-
     [TestClass]
     public class GameTest
     {
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void TestPointOutOfRange()
+        {
+            var game = new Game();
+            Debug.Print(game.Attack(new Point { Row = 11, Column = 10 }));
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void TestShipAlreadyThere()
+        {
+            var game = new Game();
+            Debug.Print(game.CurrentPlayerName);
+            game.AddShip(new Ship { Orientation = Orientation.Horizontal, Length = 3 }, new Point { Row = 1, Column = 1 });
+            game.AddShip(new Ship { Orientation = Orientation.Horizontal, Length = 1 }, new Point { Row = 1, Column = 3 });
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void TestShipDoesntFit()
+        {
+            var game = new Game();
+            Debug.Print(game.CurrentPlayerName);
+            game.AddShip(new Ship { Orientation = Orientation.Horizontal, Length = 2 }, new Point { Row = 10, Column = 10 });
+        }
+
         [TestMethod]
         public void TestScenario()
         {
@@ -35,6 +62,8 @@ namespace BattleshipUnitTests
             Debug.Print(game.CurrentPlayerName);
             Debug.Print(game.Attack(new Point { Row = 2, Column = 2 }));
             Assert.IsTrue(game.HasWon);
+
+            if (game.HasWon) Debug.Print(string.Format("{0} has won", game.CurrentPlayerName));
         }
     }
 }
