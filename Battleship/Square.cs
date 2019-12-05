364d4e3540573006924be1e5b13c2c1912f8f2c1
Laurent Beaumer <laurent.beaumer@free.fr>
2019-12-05 12:31:48 +1100

    Battleship.Square: delete (not needed anymore)

--- Battleship/Square.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Battleship
-{
-    public enum Square
-    {
-        Empty,      // default state
-        Occupied,   // ship has been added
-        Hit         // ship has been added
-    }
-}
